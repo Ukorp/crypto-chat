90fd20636a5714ad1da4d747dadd620c8a366699
Timur Safin <121217900+Ukorp@users.noreply.github.com>
2025-07-02 10:19:36 +0300

    Delete frontend directory

--- frontend/src/main/java/com/starter/demo1/crypto/KeyGenerator.java
+++
@@ -1,8 +0,0 @@
-package com.starter.demo1.crypto;
-
-import java.util.List;
-
-public interface KeyGenerator {
-
-    List<byte[]> generateRoundKeys(byte[] key);
-}
--- frontend/src/main/java/com/starter/demo1/crypto/mode/EncodeOrder.java
+++
@@ -1,5 +0,0 @@
-package com.starter.demo1.crypto.mode;
-
-public enum EncodeOrder {
-    LITTLE_ENDIAN, BIG_ENDIAN
-}
